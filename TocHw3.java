57f45ea6f5f85a069fd8c177bcb52822a7de9ae3
jeremytl <mngg030029@gmail.com>
2014-06-25 00:00:39 +0800

    debug2

--- TocHw3.java
+++ TocHw3.java
@@ -8,8 +8,8 @@
  */
 import java.io.*;
 import java.net.URL;
-import java.util.Scanner;
-import java.util.regex.Matcher;
+
+//import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.json.*;
@@ -19,59 +19,56 @@ public class TocHw3 {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException, JSONException {
 
-		if (args.length < 4) {
-			System.out.println("Too few arguments !");
-			return;
-		} else if (Integer.parseInt(args[3]) >= 104) {
-			System.out
-					.println("You can't find the trading data in the future !");
-			return;
-		}
+		try {
 
-		BufferedReader readjson = new BufferedReader(new InputStreamReader(
-				new URL(args[0]).openStream(), "UTF-8"));
-		String jsontext = new String("");
-		Scanner scan = new Scanner(readjson);
-		while (scan.hasNext())
-			jsontext += scan.nextLine();
+			BufferedReader readjson = new BufferedReader(new InputStreamReader(
+					new URL(args[0]).openStream(), "UTF-8"));
+			StringBuilder jsontext = new StringBuilder();
+			String curline;
+			while ((curline = readjson.readLine()) != null)
+				jsontext.append(curline);
 
-		// jsontext+="]";
+			// jsontext+="]";
 
-		scan.close();
-		readjson.close();
+			readjson.close();
+			// System.out.println("WTF why I parse so much long time using String's "+=" without StringBuilder.append!!!");
+			// System.out.println(jsontext);
 
-		// System.out.println(jsontext);
+			JSONArray realpricejson = new JSONArray(jsontext.toString());
 
-		JSONArray realpricejson = new JSONArray(jsontext);
+			Pattern sectionp = Pattern.compile(args[1]);
+			Pattern locationp = Pattern.compile(args[2]);
 
-		Pattern sectionp = Pattern.compile(args[1]);
-		Pattern locationp = Pattern.compile(args[2]);
+			int i, avg_price = 0, cnt = 0;
+			int earliesty = Integer.parseInt(args[3]) * 100;
 
-		int i, avg_price = 0, cnt = 0;
-		int earliesty = Integer.parseInt(args[3]) * 100;
-		int matchery;
-
-		JSONObject aline = realpricejson.getJSONObject(0);
-		for (i = 0; i < realpricejson.length(); i++) {
-			aline = realpricejson.getJSONObject(i);
-			matchery = aline.getInt("交易年月");
-			if (sectionp.matcher(aline.getString("鄉鎮市區")).find()
-					&& locationp.matcher(aline.getString("土地區段位置或建物區門牌"))
-							.find() && matchery > earliesty) {
-				avg_price += aline.getInt("總價元");
-				cnt++;
+			for (i = 0; i < realpricejson.length(); i++) {
+				if (sectionp.matcher(
+						realpricejson.getJSONObject(i).getString("鄉鎮市區"))
+						.find()
+						&& locationp.matcher(
+								realpricejson.getJSONObject(i).getString(
+										"土地區段位置或建物區門牌")).find()
+						&& realpricejson.getJSONObject(i).getInt("交易年月") > earliesty) {
+					avg_price += realpricejson.getJSONObject(i).getInt("總價元");
+					cnt++;
+				}
 			}
-		}
 
-		if (cnt != 0) {
-			avg_price /= cnt;
-			System.out.println(avg_price);
-		} else {
-			System.out.println("No data matches " + args[1] + args[2]
-					+ " and trade at/atfer " + args[3]);
+			if (cnt != 0) {
+				avg_price /= cnt;
+				System.out.println(avg_price);
+			} else {
+				System.out.println("No data matches " + args[1] + args[2]
+						+ " and trade at/after " + args[3]);
+			}
+		} catch (IOException e) {
+			
+				System.out.println("Too few arguments or error argument format!");
+				e.printStackTrace();
+				
 		}
 
 	}
